051375a42a90fa3bdf5b766a9cba7eb4cad1768f
kluufger <nobody@edify.space>
2018-04-26 06:41:43 +0000

    init

---
+++ Tarradiddle/Tarradiddle.h
@@ -0,0 +1,19 @@
+//
+//  Tarradiddle.h
+//  Tarradiddle
+//
+//  Created by asd on 4/26/18.
+//  Copyright © 2018 noose&needle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tarradiddle.
+FOUNDATION_EXPORT double TarradiddleVersionNumber;
+
+//! Project version string for Tarradiddle.
+FOUNDATION_EXPORT const unsigned char TarradiddleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tarradiddle/PublicHeader.h>
+
+
