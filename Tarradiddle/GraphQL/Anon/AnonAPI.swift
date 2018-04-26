e44c083470eb6392e97ab99839b5bb2e78456859
kluufger <nobody@edify.space>
2018-04-26 09:02:46 +0000

    add CreateUser mutation

---
+++ Tarradiddle/GraphQL/Anon/AnonAPI.swift
@@ -0,0 +1,130 @@
+//  This file was automatically generated and should not be edited.
+
+import Apollo
+
+public final class CreateUserMutation: GraphQLMutation {
+
+  public static let operationString =
+    "mutation CreateUser($icloudId: String!) {\n  createUser(icloudId: $icloudId) {\n    __typename\n    token\n    user {\n      __typename\n      id\n    }\n  }\n}"
+
+  public var icloudId: String
+
+  public init(icloudId: String) {
+    self.icloudId = icloudId
+  }
+
+  public var variables: GraphQLMap? {
+    return ["icloudId": icloudId]
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes = ["RootMutationType"]
+
+    public static let selections: [GraphQLSelection] = [
+      GraphQLField("createUser", arguments: ["icloudId": GraphQLVariable("icloudId")], type: .object(CreateUser.selections)),
+    ]
+
+    public var snapshot: Snapshot
+
+    public init(snapshot: Snapshot) {
+      self.snapshot = snapshot
+    }
+
+    public init(createUser: CreateUser? = nil) {
+      self.init(snapshot: ["__typename": "RootMutationType", "createUser": createUser.flatMap { (value: CreateUser) -> Snapshot in value.snapshot }])
+    }
+
+    public var createUser: CreateUser? {
+      get {
+        return (snapshot["createUser"] as? Snapshot).flatMap { CreateUser(snapshot: $0) }
+      }
+      set {
+        snapshot.updateValue(newValue?.snapshot, forKey: "createUser")
+      }
+    }
+
+    public struct CreateUser: GraphQLSelectionSet {
+      public static let possibleTypes = ["Account"]
+
+      public static let selections: [GraphQLSelection] = [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("token", type: .scalar(String.self)),
+        GraphQLField("user", type: .object(User.selections)),
+      ]
+
+      public var snapshot: Snapshot
+
+      public init(snapshot: Snapshot) {
+        self.snapshot = snapshot
+      }
+
+      public init(token: String? = nil, user: User? = nil) {
+        self.init(snapshot: ["__typename": "Account", "token": token, "user": user.flatMap { (value: User) -> Snapshot in value.snapshot }])
+      }
+
+      public var __typename: String {
+        get {
+          return snapshot["__typename"]! as! String
+        }
+        set {
+          snapshot.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var token: String? {
+        get {
+          return snapshot["token"] as? String
+        }
+        set {
+          snapshot.updateValue(newValue, forKey: "token")
+        }
+      }
+
+      public var user: User? {
+        get {
+          return (snapshot["user"] as? Snapshot).flatMap { User(snapshot: $0) }
+        }
+        set {
+          snapshot.updateValue(newValue?.snapshot, forKey: "user")
+        }
+      }
+
+      public struct User: GraphQLSelectionSet {
+        public static let possibleTypes = ["User"]
+
+        public static let selections: [GraphQLSelection] = [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+        ]
+
+        public var snapshot: Snapshot
+
+        public init(snapshot: Snapshot) {
+          self.snapshot = snapshot
+        }
+
+        public init(id: GraphQLID) {
+          self.init(snapshot: ["__typename": "User", "id": id])
+        }
+
+        public var __typename: String {
+          get {
+            return snapshot["__typename"]! as! String
+          }
+          set {
+            snapshot.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var id: GraphQLID {
+          get {
+            return snapshot["id"]! as! GraphQLID
+          }
+          set {
+            snapshot.updateValue(newValue, forKey: "id")
+          }
+        }
+      }
+    }
+  }
+}
