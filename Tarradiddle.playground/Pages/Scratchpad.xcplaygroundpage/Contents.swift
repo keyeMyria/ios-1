1582e3c13f6bd9ae06985e09e7317cc74fc6a07d
kluufger <nobody@edify.space>
2018-04-26 09:33:55 +0000

    update playgrounds

--- Tarradiddle.playground/Pages/Scratchpad.xcplaygroundpage/Contents.swift
+++ Tarradiddle.playground/Pages/Scratchpad.xcplaygroundpage/Contents.swift
@@ -2,12 +2,25 @@
 
 import RxSwift
 
-Observable.from(["hello", "darkness", "my", "old", "friend"])
-  .map { $0.uppercased() }
-  .subscribe(onNext: { print($0) })
+//Observable.from(["hello", "darkness", "my", "old", "friend"])
+//  .map { $0.uppercased() }
+//  .subscribe(onNext: { print($0) })
 
 import Apollo
 import RxApollo
+@testable import Tarradiddle
 
 let apollo = ApolloClient(url: URL(string: "http://localhost:4000/graphql")!)
-apollo.rx.perform(mutation: <#T##GraphQLMutation#>)
+apollo.rx
+  .perform(mutation: CreateUserMutation(icloudId: "from xcode"))
+  .subscribe(onSuccess: { data in
+    print(data.createUser)
+  }, onError: { error in
+    print(error)
+  }, onCompleted: {
+    print("completed")
+  })
+
+apollo.perform(mutation: CreateUserMutation(icloudId: "from xcode")) { result, error in
+  print(result)
+}
