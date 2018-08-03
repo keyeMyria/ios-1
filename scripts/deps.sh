df971457f386933fa2f12e31f5127b19285faa9d
kluufger <nobody@edify.space>
2018-08-03 06:19:51 +0000

    update deps.sh

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
-# if [ -d "Carthage" ]; then
-carthage update --new-resolver --platform ios --no-use-binaries --no-build
-rm -rf Carthage/Checkouts/GRDB.swift/GRDBCipher.xcodeproj
-rm -rf Carthage/Checkouts/GRDB.swift/GRDBCustom.xcodeproj
-rm -rf Carthage/Checkouts/apollo-ios/ApolloSQLite.xcodeproj
-rm -rf Carthage/Checkouts/apollo-ios/ApolloWebSocket.xcodeproj
-rm -rf Carthage/Checkouts/Starscream
-rm -rf Carthage/Checkouts/SQLite.swift
-carthage build --platform ios --cache-builds
-# fi
+set -e
+
+function install_ios_frameworks() {
+  carthage update --new-resolver --platform ios --no-use-binaries --no-build
+  rm -rf Carthage/Checkouts/GRDB.swift/GRDBCipher.xcodeproj
+  rm -rf Carthage/Checkouts/GRDB.swift/GRDBCustom.xcodeproj
+  rm -rf Carthage/Checkouts/apollo-ios/ApolloSQLite.xcodeproj
+  rm -rf Carthage/Checkouts/apollo-ios/ApolloWebSocket.xcodeproj
+  rm -rf Carthage/Checkouts/Starscream
+  rm -rf Carthage/Checkouts/SQLite.swift
+  carthage build --platform ios --cache-builds
+}
+
+install_ios_frameworks
+
+set +e
